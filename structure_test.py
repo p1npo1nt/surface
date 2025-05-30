d53bc36d4a3cc53ec03965483b71ab6bdb893799
Vasudevan Govardhanen <48339921+p1npo1nt@users.noreply.github.com>
2025-05-30 10:23:44 -0400

    Update structure_test.py

--- structure_test.py
+++ structure_test.py
@@ -1,1 +1,22 @@
+from vpython import rate, vec, canvas, color
+from structures import Vertex, Edge
+
+scene = canvas()
+
+#2x2 grid
+vertices = [
+    Vertex(vec(x, 0, z), 1.0, fixed=(x==0 or x==1))
+    for z in range(2) for x in range(2)
+]
+
+edges = [
+    Edge(vertices[0], vertices[1]),
+    Edge(vertices[0], vertices[2]),
+    Edge(vertices[1], vertices[3]),
+    Edge(vertices[2], vertices[3]),
+    Edge(vertices[0], vertices[3])  #diagonal spring
+]
+
+dt = 0.01
+g = vec(0, -9.8, 0)
 
